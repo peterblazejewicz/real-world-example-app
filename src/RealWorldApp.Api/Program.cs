62e895e5a06222ae15c5a7bf8ddd403527d32357
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-10-11 22:35:16 +0200

    Fix typo in project namespace

--- src/RealWorldApp.Api/Program.cs
+++ src/RealWorldApp.Api/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 
-namespace RealWolrdApp.Api
+namespace RealWorldApp.Api
 {
     public class Program
     {
--- src/RealWorldApp.Api/Startup.cs
+++ src/RealWorldApp.Api/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace RealWolrdApp.Api
+namespace RealWorldApp.Api
 {
     public class Startup
     {
