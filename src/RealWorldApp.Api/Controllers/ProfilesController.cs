de13ffabe43d77bd9098cf6019d08b30610de3e2
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-10-11 22:41:25 +0200

    Controllers structure
    - articles
    - profiles
    - tags
    - users

--- src/RealWorldApp.Api/Controllers/ProfilesController.cs
+++ src/RealWorldApp.Api/Controllers/ProfilesController.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
 namespace RealWorldApp.Api.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class ProfilesController : Controller
     {
         // GET api/values
         [HttpGet]
